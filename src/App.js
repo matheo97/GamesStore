99c7dc3199ed119d0ba8bdb30e0b65c61dac0c02
Mate Salazar <matt9742@Mates-iMac.local>
2019-09-28 19:28:36 -0500

    Init Project

--- src/App.js
+++ src/App.js
@@ -1,26 +1,18 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import { Provider as ReduxProvider } from "react-redux";
+import GamingRouter from "../src/routes/index";
+import configureStore from "./shared/setup/store";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const reduxStore = configureStore(window.REDUX_INITIAL_DATA);
+
+class App extends Component {
+  render() {
+    return (
+      <ReduxProvider store={reduxStore}>
+        <GamingRouter />
+      </ReduxProvider>
+    );
+  }
 }
 
 export default App;
---
+++ src/actions/index.js
@@ -0,0 +1,1 @@
+export { default as loadGames } from './home/loadGames';
